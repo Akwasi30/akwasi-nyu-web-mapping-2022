2bf253be7c69a0259e57c66cc0c95d161444c848
Akwasi30 <98895595+Akwasi30@users.noreply.github.com>
2022-02-06 08:12:55 -0500

    first commit after porting jsfiddle to separate files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$('#newark-ewr').on('click', function() {
+	$(".right-half").html("<img src='https://www.airport-ewr.com/images/newark-airport-aerial.jpg'/>");
+});
+$('#laguardia-airport-redevelopment').on('click', function() {
+	$(".right-half").html("<img src='https://304ljw4amcep3ali496xph6b-wpengine.netdna-ssl.com/wp-content/uploads/2020/04/LGA-Master-Plan-1900.jpg'/>");
+})
+$('#delta-jfk-redevelopment-program').on('click', function() {
+	$(".right-half").html("<img src='https://images.adsttc.com/media/images/61bf/0de6/1c01/5968/a16a/89ee/slideshow/jfk-t1-exteriorview-2.jpg?1639910896'/>");
+})
